89706a39e6937807cafc84e3943fd1461ec030b4
Yuliya <yuliya_larionova@inbox.ru>
2021-09-17 12:54:27 +0300

    Remove redundant function

--- salary.py
+++ salary.py
@@ -1,16 +1,8 @@
-import numpy
-
-
 def predict_salary(salary_from, salary_to):
-    if salary_to is None:
+    if not salary_to:
         salary = salary_from * 1.2
-    elif salary_from is None:
+    elif not salary_from:
         salary = salary_to * 0.8
     else:
         salary = (salary_from + salary_to) / 2
     return salary
-
-
-def predict_average_salary(salary_list):
-    avg_salary = int(numpy.average(salary_list))
-    return avg_salary
