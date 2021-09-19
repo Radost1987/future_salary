4f4d6adfb68ada7df5c8d09dbd620f7447ae7fa3
Yuliya <yuliya_larionova@inbox.ru>
2021-09-20 00:10:19 +0300

    Remove average salary calculation

--- hh_statistics.py
+++ hh_statistics.py
@@ -1,8 +1,7 @@
-import numpy
 import requests
 from itertools import count
 
-from salary import predict_salary
+from salary import predict_salary, get_average_salary
 
 
 def download_hh_vacancies(language):
@@ -34,25 +33,14 @@ def predict_rub_salary_hh(vacancy):
         return salary
 
 
-def get_average_salary(vacancies):
-    salary_list = []
-    for vacancy in vacancies:
-        salary = predict_rub_salary_hh(vacancy)
-        if salary:
-            salary_list.append(salary)
-    if salary_list:
-        average_salary = int(numpy.average(salary_list))
-    else:
-        average_salary = 0
-    vac_processed = len(salary_list)
-    return average_salary, vac_processed
-
-
 def collect_hh_statistics(languages):
     language_statistics = {}
     for language in languages:
         number_founded_vacancies, hh_vacancies = download_hh_vacancies(language)
-        average_salary, vac_processed = get_average_salary(hh_vacancies)
+        average_salary, vac_processed = get_average_salary(
+            hh_vacancies,
+            predict_rub_salary_hh
+        )
         language_statistics[language] = {
             'vacancies_found': number_founded_vacancies,
             'vacancies_processed': vac_processed,
